046950597a70a124bd17722197723d5b2cb81123
Cris Mihalache <cris.mihalache@spectrumit.eu>
2015-02-22 13:48:08 +0100

    Faster sigmoid function

--- src/neural/utility.cpp
+++ src/neural/utility.cpp
@@ -6,5 +6,6 @@ double nn_util_genrandom() {
 }
 
 double nn_util_sigmoid(double t) {
-  return 1.0 / (1.0 + exp(-t));
+  // return 1.0 / (1.0 + exp(-t));
+  return (t / ((1.0 + abs(t)) * 2.0)) + 0.5; // Faster
 }
